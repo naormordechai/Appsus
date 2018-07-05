3fc3c11d5097406cff0f40c98560064fc0b17992
unknown <naormordechaix@gmail.com>
2018-07-05 15:39:45 +0300

    changed

--- js/routes.js
+++ js/routes.js
@@ -1,7 +1,7 @@
 
-import mainApp from './pages/main-app-cmp'
-import misterKeep from './pages/mister-keep-cmp.js'
-import misterEmail from './pages/mister-email-cmp.js'
+import mainApp from './pages/main-app-cmp.js'
+import misterKeep from './pages/notes-pages/notes-cmp.js'
+import misterEmail from './pages/email-pages/mister-email-cmp.js'
 
 export default [
     {path: '/', component: mainApp},
